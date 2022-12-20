4839a130bc56861a3757a663f31420729f7d13bb
GuillaumeGraph <guillaume.caron28@gmail.com>
2022-12-20 19:49:45 +0100

    Dev_Swipecard_Base

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+
+    public Card card;
+    BoxCollider2D thisCard;
+    public bool isMouseOver;
+    private void Start()
+    {
+        thisCard = gameObject.GetComponent<BoxCollider2D>();
+    }
+    private void OnMouseOver()
+    {
+        isMouseOver = true;
+    }
+    private void OnMouseExit()
+    {
+        isMouseOver = false;
+    }
+
+}
+
+
+
+public enum CardSprite
+{
+    WATER,
+    FIRE,
+    GRASS
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Gameobjects
+    public GameObject cardGameObject;
+    public CardController mainCardController;
+    public SpriteRenderer cardSpriteRenderer;
+    public ResourceManager resourceManager;
+    //Tweawking variables
+    public float fMovingSpeed;
+    public float fSideMargin;
+    public float fSideTrigger;
+    public float divideValue;
+    float alphaText;
+    public Color textColor;
+    Vector3 pos;
+    //UI
+    public TMP_Text display;
+    public TMP_Text characterDialogue;
+    public TMP_Text actionQuote;
+    //Card variables
+    private string leftQuote;
+    private string rightQuote;
+    public Card currentCard;
+    public Card testCard;
+    void Start()
+    {
+        LoadCard(testCard);
+    }
+
+    void UpdateDialogue()
+    {
+        actionQuote.color = textColor;
+        if (cardGameObject.transform.position.x < 0)
+        {
+            actionQuote.text = leftQuote;
+        }
+        else
+        {
+            actionQuote.text = rightQuote;
+        }
+    }
+
+    void Update()
+    {
+        //Dialogue text handing
+        textColor.a = Mathf.Min((Mathf.Abs(cardGameObject.transform.position.x) - fSideMargin) / divideValue, 1);
+        if (cardGameObject.transform.position.x > fSideTrigger)
+        {
+            
+            if(Input.GetMouseButtonUp(0))
+            {
+                currentCard.Right();
+                NewCard();
+            }
+        }
+        else if(cardGameObject.transform.position.x > fSideMargin)
+        {
+
+        }
+        else if (cardGameObject.transform.position.x > -fSideMargin)
+        {
+            textColor.a = 0;
+            
+        }
+        else if (cardGameObject.transform.position.x > -fSideTrigger)
+        {
+
+        }
+        else
+        {
+            UpdateDialogue();
+            if (Input.GetMouseButtonUp(0))
+            {
+                currentCard.Left();
+                NewCard();
+            }
+        }
+        UpdateDialogue();
+        //Movement
+        if (Input.GetMouseButton(0) && mainCardController.isMouseOver)
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            cardGameObject.transform.position = pos;
+        }
+        else
+        {
+            cardGameObject.transform.position = Vector2.MoveTowards(cardGameObject.transform.position, new Vector2(0, 1), fMovingSpeed);
+        }
+        
+
+        //UI
+        display.text = "" + textColor.a;
+
+    }
+    
+
+    public void LoadCard(Card card)
+    {
+        cardSpriteRenderer.sprite = resourceManager.sprites[(int)card.sprite];
+        leftQuote = card.leftQuote;
+        rightQuote = card.rightQuote;
+        currentCard = card;
+        characterDialogue.text = card.dialogue;
+    }
+
+
+    public void NewCard()
+    {
+        int rollDice = Random.Range(0, resourceManager.cards.Length);
+        LoadCard(resourceManager.cards[rollDice]);
+    }
+}
